1712acaeac2d8e6b99f62df176cb741ceec7396a
ParticleG <particle_g@outlook.com>
2023-02-27 15:45:25 +0800

    - Try different vibrations

--- vibrate.lua
+++ vibrate.lua
@@ -1,12 +1,15 @@
-local level={0,0,.01,.016,.023,.03,.04,.05,.06,.07,.08,.09,.12,.15}
+local level={
+    Android={0,  0,.01,.016,.023,.03,.04,.05,.06,.07,.08,.09,.12,.15,.18,.21,.24,.27,.30,.33,.36},
+        iOS={0,.05,.10,.15, .20, .25,.30,.35,.40,.45,.50,.55,.60,.65,.70,.75,.80,.85,.90,.95,1  },
+}
 local vib=love.system.vibrate
 return love.system.getOS()=='iOS' and
-    function(t)
-        t=level[t]
-        if t then vib(t<=.03 and 1 or t<=.09 and 2 or 3) end
+    function(t, type)
+        t=level["iOS"][t]
+        if t then vib(t, type) end
     end
 or
     function(t)
-        t=level[t]
+        t=level["Android"][t]
         if t then vib(t) end
     end
